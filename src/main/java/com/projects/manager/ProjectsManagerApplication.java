10f0d08d6dfa36fe008548b9b215fd29368d9a98
Warrior-Player <@Ayoub-Marzouki>
2025-12-12 11:46:04 +0100

    Initialization of the project + config of actuator and database

---
+++ src/main/java/com/projects/manager/ProjectsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.projects.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsManagerApplication.class, args);
+	}
+
+}
